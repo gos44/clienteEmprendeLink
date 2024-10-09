1897923a168c73930c8ff1f71778caf75ec0e1a2
k2035ortiz <kevinalexisortizcalvahe@gmail.com>
2024-10-09 08:20:21 -0500

    subir kk

--- resources/views/publish_Entrepreneurships/show.blade.php
+++ resources/views/publish_Entrepreneurships/show.blade.php
@@ -2,23 +2,29 @@
 
 @section('content')
     <table>
-        <br><br>
         <thead>
             <tr>
                 <th>ID</th>
                 <th>Nombre</th>
+                <th>Número de Teléfono</th>
+                <th>Email</th>
+                <th>Descripción</th>
+                <th>Ubicación</th>
+                <th>URL</th>
+                <th>Fecha de Expiración</th>
             </tr>
         </thead>
         <tbody>
-           
             <tr>
-                <td>{{ $publishEntrepreneurships['id'] }}</td> 
-                <td>{{ $publishEntrepreneurships['entrepreneurs_id'] }}</td>
-                {{-- <td>{{ $entrepreneurList['investors_id'] }}</td> --}}
-
-
+                <td>{{ $publishEntrepreneurship['id'] ?? 'N/A' }}</td> 
+                <td>{{ $publishEntrepreneurship['name'] ?? 'N/A' }}</td> 
+                <td>{{ $publishEntrepreneurship['phone_number'] ?? 'N/A' }}</td> 
+                <td>{{ $publishEntrepreneurship['email'] ?? 'N/A' }}</td> 
+                <td>{{ $publishEntrepreneurship['description'] ?? 'N/A' }}</td> 
+                <td>{{ $publishEntrepreneurship['location'] ?? 'N/A' }}</td> 
+                <td>{{ $publishEntrepreneurship['url'] ?? 'N/A' }}</td> 
+                <td>{{ $publishEntrepreneurship['expiration_date'] ?? 'N/A' }}</td> 
             </tr>
-
         </tbody>
     </table>
 @endsection
