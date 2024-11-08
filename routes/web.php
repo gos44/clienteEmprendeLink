a19a46161176e28c53d721e80c87bc752f0dbbec
Sebastian-Paz <esdp5432@gmail.com>
2024-11-08 17:59:44 +1400

    CSDFSD

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ use App\Http\Controllers\PerfilUsuarioController;
 use App\Http\Controllers\usaurioPerfil_inversionistaController;
 
 
-
+// CONTROLADORES DEL SEBAS
 use App\Http\Controllers\Controllers_Sebas\Busqueda_Filtro_UsuarioController;
 use App\Http\Controllers\Controllers_Sebas\Busqueda_Filtro_InversionistaController;
 use App\Http\Controllers\Controllers_Sebas\Verificar_identidad_cambio_contraseña;
