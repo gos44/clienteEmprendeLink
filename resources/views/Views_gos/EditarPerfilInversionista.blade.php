51eabac3ae1515b88d48029a1043a7fc62784911
sh4ka11 <dayronhernandezparedes@gmail.com>
2024-11-18 11:08:12 -0500

    push

--- resources/views/Views_gos/EditarPerfilInversionista.blade.php
+++ resources/views/Views_gos/EditarPerfilInversionista.blade.php
@@ -14,7 +14,6 @@
     <link rel="stylesheet" href="{{ asset('css/perfilEditarInversionista.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 EditarPerfilUsuario.blade.php
 <body>
--- resources/views/Views_gos/PerfilUsuario.blade.php
+++ resources/views/Views_gos/PerfilUsuario.blade.php
@@ -10,7 +10,6 @@
 
     <link rel="stylesheet" href="http://localHost/clienteEmprendeLink/resources/css/perfil.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 
