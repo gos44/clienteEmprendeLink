78e9b0370d654c12f4c742502732265f07831e75
Grant <jorge.a200@hotmail.com>
2024-11-19 10:08:58 -0500

    commit

--- resources/views/Views_Sebas/Busqueda_Filtro_Inversionista.blade.php
+++ resources/views/Views_Sebas/Busqueda_Filtro_Inversionista.blade.php
@@ -15,7 +15,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
      <!-- Navbar container -->
