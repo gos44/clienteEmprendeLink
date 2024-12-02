4d831b350e1a9cd9f6fcf18796053f24d6f542f0
k2035ortiz <kevinalexisortizcalvahe@gmail.com>
2024-12-01 20:35:30 -0500

    dd

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\MyentrepreneurshipController;
 use App\Http\Controllers\PublishEntrepreneurshipsController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ConnectionController;
+use App\Http\Controllers\Controller_Miguel\Publicar_Emprendimiento_Controller;
 use App\Http\Controllers\Controllers_Dayron\Chat_Inversor;
 use App\Http\Controllers\Controllers_Dayron\Chat_Usuario;
 use App\Http\Controllers\Controllers_Dayron\Editar_Emprendimiento;
@@ -239,22 +240,22 @@ Route::get('emprendimientos_deportivos_inversionista',[articulos_deportivos_inve
 Route::get('Home_inversor', [Home_inversor::class, 'index'])->name('Home_inversor.index');
 //publicar emprendimiento
 
-Route::get('Publicar_Emprendimiento1', [Publicar_Emprendimiento::class, 'Publicar_Emprendimiento1'])
+Route::get('Publicar_Emprendimiento1', [Publicar_Emprendimiento_Controller::class, 'Publicar_Emprendimiento1'])
     ->name('Publicar_Emprendimiento1');
 
-Route::post('Publicar_Emprendimiento1', [Publicar_Emprendimiento::class, 'storeStep1'])
+Route::post('Publicar_Emprendimiento1', [Publicar_Emprendimiento_Controller::class, 'storeStep1'])
     ->name('Publicar_Emprendimiento1.store');
 
-Route::get('Publicar_Emprendimiento2', [Publicar_Emprendimiento::class, 'Publicar_Emprendimiento2'])
+Route::get('Publicar_Emprendimiento2', [Publicar_Emprendimiento_Controller::class, 'Publicar_Emprendimiento2'])
     ->name('Publicar_Emprendimiento2');
 
-Route::post('Publicar_Emprendimiento2', [Publicar_Emprendimiento::class, 'storeStep2'])
+Route::post('Publicar_Emprendimiento2', [Publicar_Emprendimiento_Controller::class, 'storeStep2'])
     ->name('Publicar_Emprendimiento2.store');
 
-Route::get('Publicar_Emprendimiento3', [Publicar_Emprendimiento::class, 'Publicar_Emprendimiento3'])
+Route::get('Publicar_Emprendimiento3', [Publicar_Emprendimiento_Controller::class, 'Publicar_Emprendimiento3'])
     ->name('Publicar_Emprendimiento3');
 
-Route::post('Publicar_Emprendimiento3', [Publicar_Emprendimiento::class, 'store'])
+Route::post('Publicar_Emprendimiento3', [Publicar_Emprendimiento_Controller::class, 'store'])
     ->name('Publicar_Emprendimiento3.store');
 
 // Route::get('Publicar_Emprendimiento1', [Publicar_Emprendimiento::class, 'Publicar_Emprendimiento1'])
