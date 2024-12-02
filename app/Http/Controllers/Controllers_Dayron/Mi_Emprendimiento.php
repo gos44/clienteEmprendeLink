553dc0c58e39ba9d464a267b20de7d68b31f0b4c
Sebastian-Paz <esdp5432@gmail.com>
2024-12-03 12:34:58 +1400

    modelo

--- app/Http/Controllers/Controllers_Dayron/Mi_Emprendimiento.php
+++ app/Http/Controllers/Controllers_Dayron/Mi_Emprendimiento.php
@@ -4,7 +4,7 @@
 namespace App\Http\Controllers\Controllers_Dayron;
 
 use App\Http\Controllers\Controller;
-use App\Models\Emprendimiento; // Asegúrate de que el modelo Emprendimiento esté correctamente importado
+use App\Models\Myentrepreneurship; // Asegúrate de que el modelo Emprendimiento esté correctamente importado
 use Illuminate\Http\Request;
 
 class Mi_Emprendimiento extends Controller
@@ -12,7 +12,7 @@ class Mi_Emprendimiento extends Controller
     public function show($id)
     {
         // Busca el emprendimiento por el ID
-        $emprendimiento = Emprendimiento::find($id);
+        $emprendimiento = Myentrepreneurship::find($id);
 
         if (!$emprendimiento) {
             return redirect()->route('home')->with('error', 'Emprendimiento no encontrado');
