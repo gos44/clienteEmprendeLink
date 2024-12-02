a59e74df2679055d085ca1f3463c196086a2d0a7
Sebastian-Paz <esdp5432@gmail.com>
2024-12-03 12:23:48 +1400

    sdfdsf

--- routes/web.php
+++ routes/web.php
@@ -119,7 +119,7 @@ Route::get('entrepreneurships/{id}', [EntrepreneurshipController::class, 'show']
 Route::get('myentrepreneurships', [MisEmpredimientosController::class, 'index'])->name('myentrepreneurships.index');
 
 // Ruta para obtener el detalle de un "Myentrepreneurship" específico por su ID
-Route::get('myentrepreneurshipsssse/{id}', [Mi_Emprendimiento::class, 'show'])->name('myentrepreneurships.show');
+Route::get('myentrepreneurships/{id}', [Mi_Emprendimiento::class, 'show'])->name('myentrepreneurships.show');
 
 
 
