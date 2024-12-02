dc19ead383b37e0aa77ae315b6efe38ad9bcff34
Sebastian-Paz <esdp5432@gmail.com>
2024-12-03 12:44:36 +1400

    dsasd

--- app/Http/Controllers/Controllers_Dayron/Mi_Emprendimiento.php
+++ app/Http/Controllers/Controllers_Dayron/Mi_Emprendimiento.php
@@ -11,10 +11,10 @@ class Mi_Emprendimiento extends Controller
     public function show($id)
     {
         // Busca el emprendimiento por el ID
-        $emprendimiento = Myentrepreneurship::find($id);
+        $entrepreneur = Myentrepreneurship::find($id);
 
         // Si no se encuentra el emprendimiento, redirige con un mensaje de error
-        if (!$emprendimiento) {
+        if (!$entrepreneur) {
             return redirect()->route('home')->with('error', 'Emprendimiento no encontrado');
         }
 
