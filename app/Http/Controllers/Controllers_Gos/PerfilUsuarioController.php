d9a4353dba117694c97e28f32b9957f1face02c1
GUSTAVO <Gustavoandressanchezceron@gmail.com>
2024-12-03 11:32:24 -0500

    a

--- app/Http/Controllers/Controllers_Gos/PerfilUsuarioController.php
+++ app/Http/Controllers/Controllers_Gos/PerfilUsuarioController.php
@@ -10,14 +10,12 @@ class PerfilUsuarioController extends Controller
     public function index()
     {
     
-        $response = Http::get("apiemprendelink-production-9272.up.railway.app/api/Entrepreneurs/1?included=user");
-        
+        $response = Http::get("apiemprendelink-production-9272.up.railway.app/api/Entrepreneurs/1?included=user");        
         if ($response->successful()) {
             $connections = $response->json()['data'] ?? [];
-            
             return view('Views_gos.PerfilUsuario', compact('connections'));
         } else {
-            // Manejar errores
+            // Handle errors
             return back()->with('error', 'No se pudieron cargar los datos del perfil');
         }
     }
