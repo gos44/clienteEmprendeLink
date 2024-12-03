9f855e53382e0cf6ad4ea21bd23bac0ec07bb2d3
Sebastian-Paz <esdp5432@gmail.com>
2024-12-03 18:09:36 +1400

    modelo5

--- app/Http/Controllers/Controllers_Dayron/Mi_Emprendimiento.php
+++ app/Http/Controllers/Controllers_Dayron/Mi_Emprendimiento.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Controllers_Dayron;
 
 use App\Http\Controllers\Controller;
-use App\Models\Myentrepreneurship; // Importamos el modelo correcto
+use App\Models\publish_Entrepreneurships; // Importamos el modelo correcto
 use Illuminate\Http\Request;
 
 class Mi_Emprendimiento extends Controller
@@ -11,7 +11,7 @@ class Mi_Emprendimiento extends Controller
     public function show($id)
     {
         // Busca el emprendimiento por el ID
-        $emprendimiento = Myentrepreneurship::findOrFail($id);
+        $emprendimiento = publish_Entrepreneurships::findOrFail($id);
 
         // Retorna la vista con los datos del emprendimiento
         return view('Views_Dayron.Mi_Emprendimiento', compact('emprendimiento'));
