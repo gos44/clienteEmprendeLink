2bd954d885125d991be9176625baa3b0066cd4a3
Sebastian-Paz <esdp5432@gmail.com>
2024-12-04 16:52:28 +1400

    login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('Home_Usuario.index');
+        return $request->expectsJson() ? null : route('login');
     }
 }
