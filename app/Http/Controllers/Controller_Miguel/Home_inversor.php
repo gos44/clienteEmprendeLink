a7fd6f82fab30c6cec0851ce22ea67f200724b92
Sebastian-Paz <esdp5432@gmail.com>
2024-12-05 16:58:08 +1400

    inversor

--- app/Http/Controllers/Controller_Miguel/Home_inversor.php
+++ app/Http/Controllers/Controller_Miguel/Home_inversor.php
@@ -3,22 +3,25 @@
 namespace App\Http\Controllers\Controller_Miguel;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Home_inversor extends Controller
 {
-
+    public function __construct()
+    {
+        // Aplica el middleware de autenticación
+        $this->middleware('auth');
+    }
 
     public function index()
     {
-        // // Simulación de datos que se pueden utilizar en la vista
-        // $connections = [
-        //     // Ejemplo de datos
-        //     ['name' => 'Connection 1', 'description' => 'Description of connection 1'],
-        //     ['name' => 'Connection 2', 'description' => 'Description of connection 2']
-        // ];
+        // Verifica si el usuario tiene el rol de inversionista
+        if (Auth::user()->role !== 'inversionista') {
+            // Redirige al usuario a una vista específica si no tiene permisos
+            return redirect()->route('Home1.index')->with('error', 'No tienes permiso para acceder a esta página.');
+        }
 
-        // Retorna la vista 'Perfil' con los datos de prueba
+        // Retorna la vista para los usuarios con el rol "inversionista"
         return view('views_Miguel.home_inversor');
     }
 }
