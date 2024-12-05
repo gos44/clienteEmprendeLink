d8e1e807070d53d37c25fbc678e286302b773faf
GUSTAVO <Gustavoandressanchezceron@gmail.com>
2024-12-04 22:39:30 -0500

    a

--- app/Http/Controllers/Controllers_Sebas/inicio_de_sesion_usuariocontroller.php
+++ app/Http/Controllers/Controllers_Sebas/inicio_de_sesion_usuariocontroller.php
@@ -29,7 +29,7 @@ class inicio_de_sesion_usuariocontroller extends Controller
             $request->session()->regenerate();
 
             // Redirigir al usuario a la ruta protegida
-            return redirect()->route('dashboard');
+            return redirect()->route('Home_Usuario');
         }
 
         // Si la autenticación falla, regresar con error
