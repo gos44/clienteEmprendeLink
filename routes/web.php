b023ed394b9c6a3b477d063bfe432cfe8b135b97
sh4ka11 <dayronhernandezparedes@gmail.com>
2024-12-09 18:46:38 -0500

    PRUERBA

--- routes/web.php
+++ routes/web.php
@@ -384,10 +384,8 @@ Route::get('Home_inversor', [Home_inversor::class, 'index'])->name('Home_inverso
 
 
 
-Route::middleware(['auth:api'])->group(function () {
-    Route::get('/publicar-emprendimiento', [Publicar_Emprendimiento_Controller::class, 'index'])->name('Publicar_Emprendimiento');
-    Route::post('/publicar-emprendimiento', [Publicar_Emprendimiento_Controller::class, 'guardarEmprendimiento'])->name('guardarEmprendimiento');
-});
+Route::get('/publicar-emprendimiento', [Publicar_Emprendimiento_Controller::class, 'index'])->name('Publicar_Emprendimiento');
+Route::post('/publicar-emprendimiento', [Publicar_Emprendimiento_Controller::class, 'guardarEmprendimiento'])->name('guardarEmprendimiento');
 
 
 // Route::get('Publicar_Emprendimiento1', [Publicar_Emprendimiento::class, 'Publicar_Emprendimiento1'])
