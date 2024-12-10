6395e84736f2695cb841c98fa2d5442fa859ebf5
k2035ortiz <kevinalexisortizcalvahe@gmail.com>
2024-12-10 04:15:16 -0500

    k vist

--- resources/views/Pruebas_gos/Visitar_Emprendimiento_Inversor.blade.php
+++ resources/views/Pruebas_gos/Visitar_Emprendimiento_Inversor.blade.php
@@ -100,7 +100,7 @@
             <!-- Reseñas Section -->
             <div class="hero-buttons">
                 <a href="{{ route('resenaInver') }}" class="btn btn-primary">Reseñas</a>
-                <a href="{{ route('Editar_Emprendimiento_2.index') }}" class="btn btn-secondary">Editar</a>
+                {{-- <a href="{{ route('Editar_Emprendimiento_2.index') }}" class="btn btn-secondary">Editar</a> --}}
             </div>
         </div>
         @else
