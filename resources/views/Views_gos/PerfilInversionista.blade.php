0c26c4f37ec772bea1e96565f67eed536e816e72
Sebastian-Paz <esdp5432@gmail.com>
2024-12-10 19:58:29 +1400

    boton

--- resources/views/Views_gos/PerfilInversionista.blade.php
+++ resources/views/Views_gos/PerfilInversionista.blade.php
@@ -59,8 +59,8 @@
         </div>
 
         <div class="profile-actions">
-            <form action="{{ route('perfilInver.edit', ['investor' => $user['id']]) }}" method="POST">
-                <button class="btn-primary">Editar perfil</button>
+            <a href="{{ route('perfilInver.edit', ['investor' => $user['id']]) }}">
+                <button type="button" class="btn-primary">Editar perfil</button>
             </a>
             <a href="{{ route('Home1.index') }}">
                 <button class="btn-outline">Cerrar Sesión</button>
