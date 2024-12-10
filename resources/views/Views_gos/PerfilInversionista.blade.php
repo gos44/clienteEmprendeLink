1ba60dfb3382da850d9916c2e9e0651351088265
Sebastian-Paz <esdp5432@gmail.com>
2024-12-10 19:20:44 +1400

    ruta

--- resources/views/Views_gos/PerfilInversionista.blade.php
+++ resources/views/Views_gos/PerfilInversionista.blade.php
@@ -59,7 +59,7 @@
         </div>
 
         <div class="profile-actions">
-            <a href="{{ route('perfilInver.update') }}">
+            <form action="{{ route('perfilInver.update', ['investor' => $user['id']]) }}" method="POST">
                 <button class="btn-primary">Editar perfil</button>
             </a>
             <a href="{{ route('Home1.index') }}">
